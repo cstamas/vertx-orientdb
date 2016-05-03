fc61f4c3627acd52074208298477f424a260b229
Tamas Cservenak <tamas@cservenak.net>
2016-05-03 21:31:52 +0200

    Added some handlers

--- database/src/main/java/org/cstamas/vertx/orientdb/OrientUtils.java
+++ database/src/main/java/org/cstamas/vertx/orientdb/OrientUtils.java
@@ -2,13 +2,16 @@ package org.cstamas.vertx.orientdb;
 
 import java.util.Map;
 
+import com.orientechnologies.common.concur.ONeedRetryException;
 import com.orientechnologies.orient.core.command.OCommandResultListener;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.query.OSQLNonBlockingQuery;
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
@@ -18,6 +21,62 @@ public class OrientUtils
 {
   private OrientUtils() {
     // nop
+  }
+
+  /**
+   * Wraps passed in handler into a transaction.
+   */
+  public static <T> Handler<AsyncResult<ODatabaseDocumentTx>> tx(final Handler<AsyncResult<ODatabaseDocumentTx>> handler) {
+    checkNotNull(handler);
+    return adb -> {
+      if (adb.succeeded()) {
+        ODatabaseDocumentTx db = adb.result();
+        db.begin();
+        handler.handle(adb);
+        db.commit();
+      }
+      else {
+        handler.handle(adb);
+      }
+    };
+  }
+
+  /**
+   * Retries {@link Handler} multiple times, usable with OrientDB MVCC and {@link ONeedRetryException}.
+   */
+  public static <T> Handler<AsyncResult<ODatabaseDocumentTx>> retry(final int retries,
+                                                                    final Handler<AsyncResult<ODatabaseDocumentTx>> handler)
+  {
+    checkArgument(retries > 0);
+    checkNotNull(handler);
+    return adb -> {
+      if (adb.succeeded()) {
+        int retry = 0;
+        Throwable throwable = null;
+        try {
+          while (retry < retries) {
+            try {
+              retry++;
+              handler.handle(adb);
+              throwable = null;
+            }
+            catch (ONeedRetryException e) {
+              // try again
+              throwable = e;
+            }
+          }
+        }
+        catch (Exception e) {
+          throwable = e;
+        }
+        if (throwable != null) {
+          handler.handle(Future.failedFuture(throwable));
+        }
+      }
+      else {
+        handler.handle(adb);
+      }
+    };
   }
 
   /**

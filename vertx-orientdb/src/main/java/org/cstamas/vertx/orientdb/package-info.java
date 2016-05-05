6062a5b1544a7e1711f3f82a68585085f8742f0a
Tamas Cservenak <tamas@cservenak.net>
2016-05-05 11:59:30 +0200

    Update module

--- vertx-orientdb/src/main/java/org/cstamas/vertx/orientdb/package-info.java
+++ vertx-orientdb/src/main/java/org/cstamas/vertx/orientdb/package-info.java
@@ -1,4 +1,4 @@
-@ModuleGen(name = "vertx-orient", groupPackage = "org.cstamas.vertx.orientdb")
+@ModuleGen(name = "vertx-orientdb", groupPackage = "org.cstamas.vertx.orientdb")
 package org.cstamas.vertx.orientdb;
 
 import io.vertx.codegen.annotations.ModuleGen;
